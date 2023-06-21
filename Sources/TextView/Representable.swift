3cc03e6538293df6ccb12f98a5ec1857e7a32b10
Tomas Maly <tmaly1980@gmail.com>
2023-06-21 16:01:31 -0400

    Update Representable.swift

--- Sources/TextView/Representable.swift
+++ Sources/TextView/Representable.swift
@@ -21,7 +21,7 @@ extension TextView {
         var autoDetectionTypes: UIDataDetectorTypes = []
         var allowsRichText: Bool
 
-        var onEditingChanged: ((Coordinator) -> Void)?
+        var onEditingChanged: ((TextViewProtocol) -> Void)?
         var shouldEditInRange: ((Range<String.Index>, String) -> Bool)?
         var onCommit: (() -> Void)?
 
