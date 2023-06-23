75dba3546a9586ea30e3f93ffaef2a3c43d3e6ea
Tomas Maly <tmaly1980@gmail.com>
2023-06-23 01:00:00 -0400

    Update Coordinator.swift

--- Sources/TextView/Coordinator.swift
+++ Sources/TextView/Coordinator.swift
@@ -40,7 +40,7 @@ extension TextView.Representable {
         func textViewDidChange(_ textView: UITextView) {
             let style = NSMutableParagraphStyle()
             style.lineSpacing = 40
-            let attributes = [NSParagraphStyleAttributeName : style]
+            let attributes = [NSAttributedString.Key.paragraphStyle : style]
             
             text.wrappedValue = NSAttributedString(attributedString: textView.attributedText, attributes: attributes)
             recalculateHeight()
